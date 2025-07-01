8fb1b4764536959ecb1d58d87e90461c4d821c0a
Brian Chen <chen_tmacy@foxmail.com>
2025-07-01 21:23:30 +0800

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::{
     net::TcpListener,
 };
 
+use anyhow::Result;
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -14,34 +16,37 @@ fn main() {
     //
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut reader = BufReader::new(&mut stream);
-                let mut line = String::new();
-
-                while let Ok(read_count) = reader.read_line(&mut line) {
-                    if read_count == 0 {
-                        break;
-                    }
-                    println!("{read_count}:{line}");
-                    if line.starts_with("*1") {
-                        line.clear();
-                        // read $4\r\n
-                        reader.read_line(&mut line).unwrap();
-                        println!("$4:{line}");
-                        line.clear();
-                        // read command
-                        reader.read_line(&mut line).unwrap();
-                        println!("cmmand : {line}");
-                        if line.contains("PING") {
-                            reader.get_mut().write_all(b"+PONG\r\n").unwrap();
-                        }
-                        line.clear();
-                    }
-                }
+            Ok(stream) => {
+                std::thread::spawn(|| {
+                    handle_client(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_client(mut stream: std::net::TcpStream) {
+    let mut reader = BufReader::new(&mut stream);
+    let mut line = String::new();
+
+    while let Ok(read_count) = reader.read_line(&mut line) {
+        if read_count == 0 {
+            break;
+        }
+        if line.starts_with("*1") {
+            line.clear();
+            // read $4\r\n
+            reader.read_line(&mut line).unwrap();
+            line.clear();
+            // read command
+            reader.read_line(&mut line).unwrap();
+            if line.contains("PING") {
+                reader.get_mut().write_all(b"+PONG\r\n").unwrap();
+            }
+            line.clear();
         }
     }
 }
