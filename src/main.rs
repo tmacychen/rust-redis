772aa16943870e40ebef6606a8c54df69864c2f0
Tmacy <chen_tmacy@foxmail.com>
2025-07-03 13:45:36 +0800

    rewrite code use tokio

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io::{BufRead, Cursor, ErrorKind, Read};
+use std::io::BufRead;
 
 use tokio::{
-    io::AsyncWriteExt,
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
@@ -18,11 +18,11 @@ async fn main() -> Result<()> {
     //
 
     loop {
-        let mut stream = listener.accept().await;
+        let stream = listener.accept().await;
         match stream {
             Ok((stream, _)) => {
                 tokio::spawn(async move {
-                    if let Err(e) = handle_client(&mut stream).await {
+                    if let Err(e) = handle_client(stream).await {
                         eprintln!("{e}");
                     }
                 });
@@ -34,85 +34,79 @@ async fn main() -> Result<()> {
     }
 }
 
-async fn handle_client(stream: &mut TcpStream) -> Result<()> {
+async fn handle_client(mut stream: TcpStream) -> Result<()> {
     let mut buf: [u8; 100] = [0; 100];
     loop {
-        stream.readable().await?;
+        let n = stream.read(&mut buf).await?;
+        if n == 0 {
+            println!("read size is 0,break loop !");
+            break;
+        }
 
-        match stream.try_read(&mut buf) {
-            Ok(n) => {
-                // println!("{:?} at {n} bytes", buf);
-                handle_command(&buf, stream);
-                break;
+        let mut reader = std::io::Cursor::new(buf);
+        let mut control_cmd = [0; 4];
+        std::io::Read::read_exact(&mut reader, &mut control_cmd)?;
+        println!("{}", String::from_utf8_lossy(&control_cmd));
+        let mut size = get_cmd_size(&mut reader);
+        let mut output = Vec::new();
+        let mut line = String::with_capacity(10);
+
+        if control_cmd[0] == b'*' {
+            match control_cmd[1] {
+                b'1' => {
+                    while size > 0 {
+                        line.clear();
+                        let n = reader
+                            .read_line(&mut line)
+                            .expect("read a line from reader");
+                        // reader get last include '\r\n' so size will be -2,must use i8.
+                        size = size - n as i8;
+                        if line.contains("PING") {
+                            output.extend_from_slice(b"+PONG\r\n");
+                        }
+                    }
+                }
+                b'2' => {
+                    while size > 0 {
+                        // reader get last include '\r\n' so size will be -2,must use i8.
+                        let mut n = read_a_line(&mut line, &mut reader);
+                        if line.contains("ECHO") {
+                            let content_size = get_cmd_size(&mut reader);
+                            println!("{}", content_size);
+
+                            n = n + content_size + read_a_line(&mut line, &mut reader);
+
+                            output.extend_from_slice(
+                                format!("${}\r\n{}\r\n", content_size, line).as_bytes(),
+                            );
+                        }
+                        size = size - n as i8;
+                    }
+                }
+                _ => {}
             }
-            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
-                continue;
-            }
-            Err(e) => {
-                return Err(e.into());
-            }
+        }
+
+        stream.writable().await?;
+        if let Err(e) = stream.write_all(&output).await {
+            eprintln!("Write client failed {:?}", e);
         }
     }
 
     Ok(())
 }
 
-async fn handle_command(buf: &[u8], stream: &mut TcpStream) {
-    let mut reader = Cursor::new(buf);
-    let mut control_cmd = [0; 2];
-    reader
-        .read_exact(&mut control_cmd)
-        .expect("read control cmd!");
-    println!("{:?}", control_cmd);
-    if control_cmd[0] == b'*' {
-        match control_cmd[1] {
-            b'1' => {
-                let mut line = String::with_capacity(10);
-                loop {
-                    let n = reader
-                        .read_line(&mut line)
-                        .expect("read a line from reader");
-                    if line.contains("PING") {
-                        if let Err(e) = stream.write_all(b"+PONG\r\n").await {
-                            eprintln!("Write client failed {:?}", e);
-                        }
-                    }
-                    if n == 0 {
-                        break;
-                    }
-                }
-            }
-            b'2' => {}
-            _ => {}
-        }
-    }
+fn read_a_line(line: &mut String, reader: &mut std::io::Cursor<[u8; 100]>) -> i8 {
+    line.clear();
+    reader.read_line(line).expect("read a line from reader") as i8
 }
-//         match line.trim() {
-//             "*1" => {
-//                 // read $num. num is len of args\r\n
-//                 reader.read_line(&mut line).unwrap();
-//                 line.clear();
-//                 reader.read_line(&mut line).unwrap();
-//                 if line.contains("PING") {
-//                     reader.get_mut().write_all(b"+PONG\r\n").unwrap();
-//                 }
-//                 line.clear();
-//             }
-//             "*2" => {
-//                 // read $num. num is len of args\r\n
-//                 reader.read_line(&mut line).unwrap();
-//                 reader.read_line(&mut line).unwrap();
 
-//                 if line.contains("ECHO") {
-//                     reader.read_line(&mut line).unwrap();
-//                     line.clear();
-//                     reader.read_line(&mut line).unwrap();
-
-//                     // println!("${}\r\n{}", line.trim().len(), line);
-//                     reader
-//                         .get_mut()
-//                         .write_all(format!("${}\r\n{}", line.trim().len(), line).as_bytes())
-//                         .unwrap();
-//                 }
-//                 line.clear();
-//                 }
+fn get_cmd_size(reader: &mut std::io::Cursor<[u8; 100]>) -> i8 {
+    let mut cmd_size = [0; 4];
+    std::io::Read::read_exact(reader, &mut cmd_size).expect("read cmd size !");
+    let size = String::from_utf8_lossy(&cmd_size);
+    size.get(1..2)
+        .unwrap()
+        .parse()
+        .expect("get size from str to u8")
+}
